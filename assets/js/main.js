0b049659646f1dc904b521dbfd9eb7677fe05266
Isidoro-D <marcelodantonio23@.com>
2024-11-05 00:34:33 -0300

    Feito o overlay async, falta apenas ajustar o Css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,77 @@
+const pokemonList = document.querySelector('#pokemonList');
+const overlay = document.getElementById('overlay');
+const loadMoreButton = document.getElementById('loadMoreButton');
 
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 12;
+let offset = 0;
+const maxRecords = 102;
 
-const overlay = document.getElementById('overlay')
-const nameOverlay = document.querySelector('.name__overlay')
-const liList = document.querySelector('.ol__li--list')
+loadPokemonItens(offset, limit);
 
-
-const limit = 12
-let offset = 0
-const maxRecords = 102
-loadPokemonItens(offset, limit)
-
-function loadPokemonItens (offset, limit) {
+function loadPokemonItens(offset, limit) {
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-    const newHtml = pokemons.map((pokemon) => `
-<li class="ol__li--list ${pokemon.type}">
-<span class="number">#${pokemon.number}</span>
-<span class="name">${pokemon.name}</span>
-<div class="details">
-<ol class="types">
-${pokemon.types.map(type => `<li class="type ${type}">${type}</li>`).join('')}
-</ol>
-<img src="${pokemon.photo}" 
-alt="${pokemon.name}">
-</div>
-</li>
-`
-      )
-      .join('')
-    pokemonList.innerHTML += newHtml
-  
-  })
+    const newHtml = pokemons
+      .map(pokemon => `
+        <li class="ol__li--list ${pokemon.type}" data-pokemon-number="${pokemon.number}">
+          <span class="number">#${pokemon.number}</span>
+          <span class="name">${pokemon.name}</span>
+          <div class="details">
+            <ol class="types">
+              ${pokemon.types.map(type => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
+          </div>
+        </li>
+      `)
+      .join('');
+    pokemonList.innerHTML += newHtml;
+  });
 }
 
+// Função para carregar mais itens ao clicar no botão "Load More"
 loadMoreButton.addEventListener('click', () => {
-  offset += limit
-
-  const qtdRecordNextPage = offset + limit
+  offset += limit;
+  const qtdRecordNextPage = offset + limit;
 
   if (qtdRecordNextPage >= maxRecords) {
-    const newLimite = maxRecords - offset
-    loadPokemonItens(offset, newLimite)
-    loadMoreButton.parentElement.removeChild(loadMoreButton)
+    const newLimit = maxRecords - offset;
+    loadPokemonItens(offset, newLimit);
+    loadMoreButton.parentElement.removeChild(loadMoreButton);
   } else {
-    loadPokemonItens(offset, limit)
+    loadPokemonItens(offset, limit);
   }
-})
+});
 
-pokemonList.addEventListener('click', (pokemon) =>{
-  overlay.classList.toggle('open--overlay')
-  
-  pokemon.map(pokemon =>
-    `<span class='name__overlay'>${pokemon.overlay}</span>`
-  )
+// Função para exibir os detalhes do Pokémon no overlay
+function showCard(pokemon) {
+  overlay.querySelector('.name__overlay').innerText = pokemon.name;
+  overlay.querySelector('.number__overlay').innerText = `#${pokemon.number}`;
+  overlay.querySelector('.type__overlay').innerText = pokemon.types[0];
+  overlay.querySelector('.type2__overlay').innerText = pokemon.types[1] || '';
+  overlay.querySelector('.img__overlay').src = pokemon.photo;
+  overlay.querySelector('.Species__status span').innerText = pokemon.species;
+  overlay.querySelector('.Height__status span').innerText = pokemon.height;
+  overlay.querySelector('.Weight__status span').innerText = pokemon.weight;
+  overlay.querySelector('.Abilities__status span').innerText = pokemon.abilities;
 
-})
+  // Exibe o overlay
+  overlay.classList.add('open--overlay');
+}
 
+// Adiciona um evento de clique para cada item da lista de Pokémon
+pokemonList.addEventListener('click', (event) => {
+  const pokemonItem = event.target.closest('li[data-pokemon-number]');
 
+  if (pokemonItem) {
+    const pokemonNumber = pokemonItem.getAttribute('data-pokemon-number');
+    pokeApi.getPokemonDetail({ url: `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}` })
+      .then(showCard);
+  }
+});
 
+// Fecha o overlay ao clicar fora dele
+overlay.addEventListener('click', (event) => {
+  if (event.target === overlay) {
+    overlay.classList.remove('open--overlay');
+  }
+});
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,37 +1,36 @@
-const pokeApi = {}
+const pokeApi = {};
 
-function convertPokeApiDetailToPokemon (pokeDetail) {
-  const pokemon = new Pokemon()
-  pokemon.number = pokeDetail.id
-  pokemon.name = pokeDetail.name
-  // pokemon.abilities = pokeDetail.abilities.ability.name
-  // pokemon.height = pokeDetail.height
-  // pokemon.weight = pokeDetail.weight
+function convertPokeApiDetailToPokemon(pokeDetail) {
+  const pokemon = new Pokemon();
+  pokemon.number = pokeDetail.id;
+  pokemon.name = pokeDetail.name;
+  pokemon.height = pokeDetail.height;
+  pokemon.weight = pokeDetail.weight;
+  pokemon.abilities = pokeDetail.abilities.map(ability => ability.ability.name).join(', ');
+  pokemon.species = pokeDetail.species.name;
 
-  const types = pokeDetail.types.map(typeSlot => typeSlot.type.name)
-  const [type] = types
+  const types = pokeDetail.types.map(typeSlot => typeSlot.type.name);
+  pokemon.types = types;
+  pokemon.type = types[0];
 
-  pokemon.types = types
-  pokemon.type = type
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
 
-  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
-  return pokemon
+  return pokemon;
 }
 
-pokeApi.getPokemonDetail = pokemon => {
+pokeApi.getPokemonDetail = (pokemon) => {
   return fetch(pokemon.url)
     .then(response => response.json())
-    .then(convertPokeApiDetailToPokemon)
-}
+    .then(convertPokeApiDetailToPokemon);
+};
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
-  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
   return fetch(url)
     .then(response => response.json())
     .then(jsonBody => jsonBody.results)
     .then(pokemons => pokemons.map(pokeApi.getPokemonDetail))
     .then(detailRequests => Promise.all(detailRequests))
-    .then(pokemonDetails => pokemonDetails)
-}
+    .then(pokemonDetails => pokemonDetails);
+};
